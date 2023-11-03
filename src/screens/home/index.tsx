a837758de9f23f7e788e3e9294355c6b9fd3b8d6
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-11-03 18:58:11 -0300

    feat(Home): header according to figma, finished

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,22 @@
 import { styles } from "./styles";
-    import { Text, View } from "react-native";
-    export default function Home(){
-      return(
+import { Text, View, Image, TextInput } from "react-native";
+
+import Logo from "../../../assets/Logo.png";
+
+export default function Home() {
+    return (
         <View style={styles.container}>
-          <Text>Hello World!</Text>
+            <View style={styles.containerHeader}>
+                <Image source={Logo} />
+            </View>
+            <View style={styles.containerMain}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Add a new task..."
+                    placeholderTextColor="#808080"
+                />
+
+            </View>
         </View>
-      )
-    }
+    )
+}
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,11 +1,33 @@
 import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#131016",
-      padding: 24
+    container:{
+        flex: 1,
+        backgroundColor: "#1E1E1E"
+    },
+    containerHeader: {
+        height: 200,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0D0D0D",
+        paddingHorizontal: 24
+    },
+    containerMain:{
+        position: "relative",
+        marginHorizontal: 24,
+        
+    }, 
+    textInput: {
+        width: "100%",
+        position: "absolute",
+        top: -25,
+        height: 54,
+        backgroundColor: "#262626",
+        padding: 16,
+        borderRadius: 6,
+        shadowOffset: { width: 0, height: 1 },
+        shadowColor: '#000',
+        shadowRadius: 2,
+        shadowOpacity: 0.5,
     }
 })
